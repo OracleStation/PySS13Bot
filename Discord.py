0fab2eecca39e6f9010c6d90df5b724ff3e58098
Birdtalon <birdtalon@gmail.com>
2018-04-16 17:29:23 +0100

    Trims messages to 2000 characters (#4)
    * sends message at 1600 characters
    
    * trims messages with a function instead
    
    * simpler check for message length

--- Discord.py
+++ Discord.py
@@ -76,6 +76,11 @@ async def on_message(message):
                 if result:
                     output = "Notes for player " + ckey + "\n\n"
                     for line in result:
+                        if len(output + line) > 2000:
+                            """If the message would be over 2000 characters then output the message and then reset"""
+                            await client.send_message(message.channel. message)
+                            output + ""
+
                         output += "``` " + line[0] + "\n"
                         output += "added at " + str(line[1]) + " by " + line[2] + "\n\n"
                         output += "```"
